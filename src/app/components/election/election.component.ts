9665d212421d51687e0855432550f803df7e9c6b
sonnyrickcruz <sonny.cruz@pointwest.com.ph>
2017-12-14 16:04:59 +0800

    fix on highlight

--- src/app/components/election/election.component.ts
+++ src/app/components/election/election.component.ts
@@ -4,6 +4,7 @@ import { VoteModel } from '../../models/vote.model';
 import { VotesSummaryModel } from '../../models/votes-summary.model';
 import { ServerSocketService } from '../../services/server-socket.service'
 import 'rxjs/add/operator/map'
+import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-election',
@@ -11,26 +12,37 @@ import 'rxjs/add/operator/map'
   styleUrls: ['./election.component.css']
 })
 export class ElectionComponent implements OnInit {
-  votesSummary: VotesSummaryModel[];
+  votesSummary: VotesSummaryModel[] = [];
   totalVotes: number;
+  objectKeys;
+  mapDisplay = {};
 
   constructor(private _serverSocket: ServerSocketService) { }
 
   ngOnInit() {
     let subscription = this._serverSocket.subscribe();
+    let isInit = false;
     subscription.subscribe((body: string) => {
-      this.votesSummary = [];
       this.totalVotes = 0;
       let result = JSON.parse(body);
-      for(let key of Object.keys(result)) {
-        this.votesSummary.push(result[key]);
-        this.totalVotes = this.totalVotes + result[key].voteCount;
+      if (!isInit) {
+        this.initBoard(result)
       }
+      for (let key of this.objectKeys) {
+        this.mapDisplay[key].voteCount = result[key].voteCount;
+        this.totalVotes += result[key].voteCount;
+      }
+      isInit = true;
     })
   }
-  
-  getPercentage(votes:number) {
-    return (votes/this.totalVotes)*100;
+
+  initBoard(result) {
+    this.objectKeys = Object.keys(result);
+    this.mapDisplay = result;
   }
-  
+
+  getPercentage(votes) {
+    return (parseInt(votes.voteCount) / this.totalVotes) * 100;
+  }
+
 }
